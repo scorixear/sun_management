8441d7aaa7ed1d2462f72cbf78a6cfa3bf530fae
MathiasKandelborg <mathias@kandelborg.dk>
2021-02-27 21:53:23 +0100

    convert buffer to string

--- src/config.js
+++ src/config.js
@@ -2,9 +2,9 @@
 import fs from 'fs'
 
 // Parse config.json file
-const config = JSON.parse(fs.readFileSync('./src/config.json'))
+const config = JSON.parse(fs.readFileSync('./src/config.json').toString())
 // add version to the object by parsing the package.json
-config.version = JSON.parse(fs.readFileSync('package.json')).version
+config.version = JSON.parse(fs.readFileSync('package.json').toString()).version
 
 // export the config object
 export default config
