8b8160804bb249887c124b82aa3e19c7583e3092
MathiasKandelborg <mathias@kandelborg.dk>
2021-02-27 21:59:56 +0100

    types are wrong

--- src/lib/messageHandler.js
+++ src/lib/messageHandler.js
@@ -1,11 +1,10 @@
-/* eslint-disable no-unused-vars */
-import Discord from 'discord.js'
+import Discord from 'discord.js';
 
-/**
- * Prints a MessageEmbed
- * @param {{msg: Discord.Message, title: string, categories: Array<{title: string, text: string, inline: boolean}>, color: number, image: string, description: string, thumbnail: string, url: string, footer: string}} param0
- * @return {Promise<(Discord.Message|Array<Discord.Message>)>}
- */
+// /**
+//  * Prints a MessageEmbed
+//  * @param {{msg: Discord.Message, title: string, categories: Array<{title: string, text: string, inline: boolean}>, color: number, image: string, description: string, thumbnail: string, url: string, footer: string}} param0
+//  * @return {Promise<(Discord.Message|Array<Discord.Message>)>}
+//  */
 function sendRichTextDefault({
   msg,
   title,
@@ -17,7 +16,7 @@ function sendRichTextDefault({
   url,
   footer
 }) {
-  return sendRichText(
+  return sendRichText({
     msg,
     title,
     categories,
@@ -27,14 +26,14 @@ function sendRichTextDefault({
     thumbnail,
     url,
     footer
-  )
+  });
 }
 
-/**
- * Prints a Message Embed
- * @param {{guild: Discord.Guild, channel: Discord.Channel, title: string, categories: Array<{title: string, text: string, inline: boolean}>, color: number, image: string, description: string, thumbnail: string, url: string, footer: string}} param0
- * @return {Promise<(Discord.Message|Array<Discord.Message>)>}
- */
+// /**
+//  * Prints a Message Embed
+//  * @param {{guild: Discord.Guild, channel: Discord.Channel, title: string, categories: Array<{title: string, text: string, inline: boolean}>, color: number, image: string, description: string, thumbnail: string, url: string, footer: string}} param0
+//  * @return {Promise<(Discord.Message|Array<Discord.Message>)>}
+//  */
 function sendRichTextDefaultExplicit({
   guild,
   channel,
@@ -48,7 +47,7 @@ function sendRichTextDefaultExplicit({
   url,
   footer
 }) {
-  return sendRichTextExplicit(
+  return sendRichTextExplicit({
     guild,
     channel,
     author,
@@ -60,25 +59,25 @@ function sendRichTextDefaultExplicit({
     thumbnail,
     url,
     footer
-  )
+  });
 }
 
-/**
- * Prints a Message Embed
- * @param {Discord.Guild} guild the Guild to print to
- * @param {Discord.Channel} channel the channel to print to
- * @param {Discord.UserResolvable} author the author of the message
- * @param {string} title the title
- * @param {Array<{title: string, test: string, inline: boolean}>} categories the fields
- * @param {number} color hex rgb color
- * @param {string} image an image path
- * @param {string} description
- * @param {string} thumbnail thumbnail url string
- * @param {string} url an url
- * @param {string} footer
- * @return {Promise<(Discord.Message|Array<Discord.Message>)>}
- */
-function sendRichTextExplicit(
+// /**
+//  * Prints a Message Embed
+//  * @param {Discord.Guild} guild the Guild to print to
+//  * @param {Discord.Channel} channel the channel to print to
+//  * @param {Discord.UserResolvable} author the author of the message
+//  * @param {string} title the title
+//  * @param {Array<{title: string, test: string, inline: boolean}>} categories the fields
+//  * @param {number} color hex rgb color
+//  * @param {string} image an image path
+//  * @param {string} description
+//  * @param {string} thumbnail thumbnail url string
+//  * @param {string} url an url
+//  * @param {string} footer
+//  * @return {Promise<(Discord.Message|Array<Discord.Message>)>}
+//  */
+function sendRichTextExplicit({
   guild,
   channel,
   author,
@@ -90,10 +89,10 @@ function sendRichTextExplicit(
   thumbnail,
   url,
   footer
-) {
-  const richText = new Discord.MessageEmbed()
+}) {
+  const richText = new Discord.MessageEmbed();
   if (title) {
-    richText.setTitle(title)
+    richText.setTitle(title);
   }
 
   if (categories) {
@@ -103,59 +102,59 @@ function sendRichTextExplicit(
           category.title,
           category.text || '',
           category.inline || false
-        )
+        );
       } else {
-        richText.addField('\u200b', '\u200b', category.inline || false)
+        richText.addField('\u200b', '\u200b', category.inline || false);
       }
-    })
+    });
   }
   if (color) {
-    richText.setColor(color)
+    richText.setColor(color);
   }
   if (description) {
-    richText.setDescription(description)
+    richText.setDescription(description);
   }
   if (thumbnail) {
     if (thumbnail.startsWith('http')) {
-      richText.setThumbnail(thumbnail)
+      richText.setThumbnail(thumbnail);
     } else {
-      richText.attachFiles([`./src/assets/${thumbnail}`])
-      richText.setThumbnail(`attachment://${thumbnail}`)
+      richText.attachFiles([`./src/assets/${thumbnail}`]);
+      richText.setThumbnail(`attachment://${thumbnail}`);
     }
   }
   if (image) {
-    richText.attachFiles([`./src/assets/${image}`])
-    richText.setImage(`attachment://${image}`)
+    richText.attachFiles([`./src/assets/${image}`]);
+    richText.setImage(`attachment://${image}`);
   }
 
   if (footer) {
-    richText.setFooter(footer)
+    richText.setFooter(footer);
   } else if (guild && author) {
-    richText.setFooter(guild.member(author).displayName, author.avatarURL())
-    richText.setTimestamp(new Date())
+    richText.setFooter(guild.member(author).displayName, author.avatarURL());
+    richText.setTimestamp(new Date());
   }
 
   if (url) {
-    richText.setURL(url)
+    richText.setURL(url);
   }
 
-  return channel.send(richText)
+  return channel.send(richText);
 }
 
-/**
- * Prints a MessageEmbed
- * @param {Discord.Message} msg the message object to print from
- * @param {string} title
- * @param {{title: string, text: string, inline: boolean}} categories the fields to add
- * @param {number} color hex rgb number
- * @param {string} image image path
- * @param {string} description
- * @param {string} thumbnail thumbnail url
- * @param {url} url
- * @param {string} footer
- * @return {Promise<(Discord.Message|Array<Discord.Message>)>}
- */
-function sendRichText(
+// /**
+// * Prints a MessageEmbed
+// * @param {Discord.Message} msg the message object to print from
+//  * @param {string} title
+//  * @param {{title: string, text: string, inline: boolean}} categories the fields to add
+//  * @param {number} color hex rgb number
+//  * @param {string} image image path
+//  * @param {string} description
+//  * @param {string} thumbnail thumbnail url
+//  * @param {url} url
+//  * @param {string} footer
+//  * @return {Promise<(Discord.Message|Array<Discord.Message>)>}
+//  */
+function sendRichText({
   msg,
   title,
   categories,
@@ -165,19 +164,20 @@ function sendRichText(
   thumbnail,
   url,
   footer
-) {
-  return sendRichTextExplicit(
-    msg.guild,
-    msg.channel,
-    msg.author,
+}) {
+  return sendRichTextExplicit({
+    guild: msg.guild,
+    channel: msg.channel,
+    author: msg.author,
     title,
     categories,
     color,
     image,
     description,
     thumbnail,
+    footer,
     url
-  )
+  });
 }
 
 export default {
@@ -185,4 +185,4 @@ export default {
   sendRichTextExplicit,
   sendRichTextDefault,
   sendRichTextDefaultExplicit
-}
+};
