39d30b700e389dd36853538998c9ab8a46e775a0
MathiasKandelborg <mathias@kandelborg.dk>
2021-02-27 22:21:42 +0100

    fix jsdoc issue & add nice semi colons

--- src/lib/languageHandler.js
+++ src/lib/languageHandler.js
@@ -1,35 +1,35 @@
-import fs from 'fs'
-import config from '../config'
+import fs from 'fs';
+import config from '../config';
 
 const dic = {
   lang: JSON.parse(
     fs.readFileSync(`./src/assets/language/${config.language}.json`).toString()
   )
-}
+};
 
 /**
  * Changes the language to the given language unicode
- * @param {String} - Language to change to
- * @return {Boolean} - False if File did not exist
+ * @param {String} lang - The language to change to
+ * @return {Boolean} - False if file did not exist
  */
 function changeLanguage(lang) {
   // Look for the given language in 'src/assets/language'
   if (!fs.existsSync(`./src/assets/language/${lang}.json`)) {
-    console.error(`Could not find language file for '${lang}'`)
-    return false
+    console.error(`Could not find language file for '${lang}'`);
+    return false;
   }
 
   // If the file exists, continue
   dic.lang = JSON.parse(
     fs.readFileSync(`./src/assets/language/${lang}.json`).toString()
-  )
+  );
 
-  console.info(`Changing lang from '${config.language}' to '${lang}'`)
-  config.language = lang
+  console.info(`Changing lang from '${config.language}' to '${lang}'`);
+  config.language = lang;
 
-  fs.writeFileSync('./src/config.json', JSON.stringify(config, null, 2))
+  fs.writeFileSync('./src/config.json', JSON.stringify(config, null, 2));
   // languageTag = lang <<< Has no use, commenting for now
-  return true
+  return true;
 }
 
 /**
@@ -39,13 +39,13 @@ function changeLanguage(lang) {
  * @return {String} the filled string
  */
 function replaceArgs(input, args) {
-  let replacedInput = input
+  let replacedInput = input;
 
   args.forEach((_, idx) => {
-    replacedInput = input.split('$' + idx).join(args[idx])
-  })
+    replacedInput = input.split('$' + idx).join(args[idx]);
+  });
 
-  return replacedInput
+  return replacedInput;
 }
 
-export { dic, changeLanguage, replaceArgs }
+export { dic, changeLanguage, replaceArgs };
