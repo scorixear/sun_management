1db30a3cc28ea397b9aa351b43e881a1f1c247bb
MathiasKandelborg <mathias@kandelborg.dk>
2021-02-27 22:04:54 +0100

    use proper types & improve readability

--- src/lib/discordHandler.js
+++ src/lib/discordHandler.js
@@ -1,48 +1,53 @@
-import Discord from 'discord.js'
-import config from '../config'
-import sqlHandler from '../misc/sqlHandler'
+import Discord from 'discord.js';
+import config from '../config';
+import sqlHandler from '../lib/sqlHandler';
 
-const client = new Discord.Client()
+const client = new Discord.Client();
 /**
  * Removes Roles from players
  * @param {Discord.Guild} guild
- * @return {Array{string}}
+ * @return {Promise<Array<String>>}}
  */
 async function removeRolesFromPlayers(guild, albionMembers) {
-  let players = []
-  let guildMembers = await guild.members.fetch()
-  for (const guildMember of guildMembers) {
+  const players = [];
+  const guildMembersCollection = await guild.members.fetch();
+  const guildMembersArr = Array.from(guildMembersCollection.flatMap);
+
+  guildMembersArr.map((guildMember) => {
     if (!guildMember[1].user.bot) {
-      // find ingame name in sql database
-      const albionName = await sqlHandler.findPlayer(guildMember[1].id)
-      // if player is not registered or ingame name is not in a sun guild
+      // find in-game name in sql database
+      const albionName = sqlHandler.findPlayer(guildMember[1].id);
+      // if player is not registered or in-game name is not in a sun guild
       if (!albionName || !albionMembers.includes(albionName)) {
-        // if ingame name is not the ignore role AND the player has at least one role
-        if (
-          albionName !== config.ignoreRole &&
+        const hasMoreThanOneRole =
           guildMember[1].roles.cache.filter((role) => role.name !== '@everyone')
-            .size > 0
-        ) {
-          // if none of the roles are one of the saveRoles
-          if (
-            !guild.roles.cache.find((role) =>
-              config.saveRoles.includes(role.name)
-            )
-          ) {
-            guildMember[1].roles.set([])
-            players.push(guildMember[1])
+            .size > 0;
+
+        // in-game name is not the ignore role AND the player has at least one role
+        if (albionName !== config.ignoreRole && hasMoreThanOneRole) {
+          // If the role is not 'safe'
+          const isNotSafeRole = !guild.roles.cache.flatMap((role) =>
+            config.safeRoles.includes(role.name)
+          );
+
+          if (isNotSafeRole) {
+            guildMember[1].roles.set([]);
+            // Add player to list
+            players.push(guildMember[1]);
           }
         }
       }
     }
-  }
-  console.log(
-    'Removed roles from ',
-    players.map((member) => member.displayName)
-  )
-  return players
+  });
+
+  console.info(
+    `Removed roles from: \n${players.map((member) => member.displayName)}`
+  );
+
+  return players;
 }
+
 export default {
   client,
   removeRolesFromPlayers
-}
+};
