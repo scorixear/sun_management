71f695775deaaca2f8857718ab80503b0f48315c
MathiasKandelborg <mathias@kandelborg.dk>
2021-03-01 19:06:41 +0100

    add nice semi colons

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
 // import packages
-import fs from 'fs'
+import fs from 'fs';
 
 // Parse config.json file
-const config = JSON.parse(fs.readFileSync('./src/config.json').toString())
+const config = JSON.parse(fs.readFileSync('./src/config.json').toString());
 // add version to the object by parsing the package.json
-config.version = JSON.parse(fs.readFileSync('package.json').toString()).version
+config.version = JSON.parse(fs.readFileSync('package.json').toString()).version;
 
 // export the config object
-export default config
+export default config;
